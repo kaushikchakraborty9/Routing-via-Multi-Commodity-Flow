804720b7a664f9eab57c9fb866d1ffab8a2f852e
kaushikchakraborty9 <43814135+kaushikchakraborty9@users.noreply.github.com>
2020-04-16 16:14:36 +0200

    Update Grap_reading.py

--- Grap_reading.py
+++ Grap_reading.py
@@ -1,5 +1,4 @@
-#import matplotlib
-#matplotlib.use('TkAgg')
+
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import networkx as nx
@@ -7,29 +6,13 @@ import scipy as sp
 from random import seed
 from random import randint
 import networkx as nx
-# seed random number generator
-
-def graph_plot(G):
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos, nodelist=G.nodes, node_color='r', node_size=500, alpha=0.8)
-    nx.draw_networkx_edges(G, pos, edgelist=G.edges, width=1.0, alpha=0.5, edge_color='b')
-    labels = nx.draw_networkx_labels(G, pos)
-    plt.axis('off')
-    plt.show()
-    plt.draw()
-
-
 
 
 
 def graph_read():
 
-
+"""Read the input graph from a .graphml.xml file. Here we consider the 'Surfnet.graphml.xml' as the input file."""
     H = nx.read_graphml('Surfnet.graphml.xml')
-   # H = nx.read_graphml('SANET.graphml.xml')
-
-    #for e in H.edges:
-    #    print(e.values())
 
     max=400
     Nodes = list(H.nodes)
@@ -41,7 +24,6 @@ def graph_read():
     for i in Nodes:
         G.add_node(int(i)+1)
 
-    #graph_plot_adv(H)
     i=0
     seed(1)
     for (n,m,w) in H.edges:
@@ -49,152 +31,32 @@ def graph_read():
         Arcs.append((int(n)+1,int(m)+1,x))
         Arcs.append((int(m)+1, int(n)+1, x))
 
-    #print(Arcs)
-
-
-
     G.add_weighted_edges_from(Arcs)
-
-
-
-
-
-
-
-    #graph_plot(G)
-
     return G
 
 
 
+
 def Mod_net(G,l):
+    
+"""Modify the original network G for applying the length constrained multi-commodity flow formulation. 
+For each node u in the original graph G = (V,E,C) we create l+1 copies of u. They are u+0, ... , u+l. 
+If two nodes u,v in the original network G is connected by an edge (u,v), then in the modified network G'=(V',E',C') 
+we will have the following edges,
+(u+0,v+1), (u+1,v+2), ... (u+(l-1),v+l)."""
 
     Nodes = []
     Arcs = []
     for i in G.nodes:
         for j in range(l+1):
             Nodes.append((i-1)*(l+1)+j+1)
-    #print(G.edges())
     for (i,j) in G.edges():
         for k in range(l):
 
             Arcs.append(((i-1)*(l+1)+k+1,(j-1)*(l+1)+k+2,list(G.edges[i,j].values())[0]))
-            #Arcs.append(( (j - 1) * (l + 1) + k + 2, (i - 1) * (l + 1) + k + 1, list(G.edges[i, j].values())[0]))
 
     return (Nodes,Arcs)
 
 
 
-
-
-
-
-def draw_graph(G,D,paths):
-    pos = nx.get_node_attributes(G, 'pos')
-    repeater_nodes = []
-    end_nodes = []
-    col = ['r','g','b','c']
-    for (i,j) in D:
-        end_nodes.append(i)
-        end_nodes.append(j)
-        #end_nodes.append((i,j))
-    for node in G.nodes():
-        if node not in end_nodes:
-            repeater_nodes.append(node)
-  #      if G.nodes[node]['type'] == 'repeater_node':
-  #          repeater_nodes.append(node)
-  #          print(node.pos)
-  #      else:
-  #          end_nodes.append(node)
-  #      i=i+1
-    fig, ax = plt.subplots(figsize=(10, 7))
-    k=0
-    for (i,j) in D:
-        ende_nodes = nx.draw_networkx_nodes(G=G, pos=pos, nodelist=[i,j], node_shape='s', node_size=700,
-                                       node_color=col[k], label="End Node")
-        ende_nodes.set_edgecolor('k')
-        k=k+1
-    rep_nodes = nx.draw_networkx_nodes(G=G, pos=pos, nodelist=repeater_nodes, node_size=600,
-                                       node_color=[[0 / 255, 166 / 255, 214 / 255]], label="Repeater Node")
-    rep_nodes.set_edgecolor('k')
-    nx.draw_networkx_labels(G=G, pos=pos, font_size=18, font_weight="bold")
-    sp_edge=[]
-
-    nx.draw_networkx_edges(G=G, pos=pos, width=1)
-    nx.draw_networkx_edges(G=G, pos=pos, edgelist = paths, width=4)
-
-    plt.axis('off')
-    margin = 0.33
-    fig.subplots_adjust(margin, margin, 1. - margin, 1. - margin)
-    ax.axis('equal')
-    fig.tight_layout()
-    plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-def graph_plot_adv(D,paths):
-    pos = {}
-    map = {}
-    color_map = []
-    end_node_list =[]
-    for (i,j) in D:
-        end_node_list.append(i)
-        end_node_list.append(j)
-        #end_node_list.append((i,j))
-
-    G = nx.read_gml('Surfnet.gml')
-
-    H = nx.Graph()
-    i=1
-    for node, nodedata in G.nodes.items():
-        #print(i,nodedata)
-        #pos[node] = [nodedata['Longitude'], nodedata['Latitude']]
-        if i ==13:
-            pos[i] = [nodedata['Longitude']+0.2, nodedata['Latitude']]
-        elif i == 40:
-            pos[i] = [nodedata['Longitude'] - 0.2, nodedata['Latitude']]
-        elif i == 45:
-            pos[i] = [nodedata['Longitude'] + 0.2, nodedata['Latitude']]
-        elif i == 38:
-            pos[i] = [nodedata['Longitude'] - 0.2, nodedata['Latitude']-0.2]
-        elif i == 24:
-            pos[i] = [nodedata['Longitude'], nodedata['Latitude'] - 0.2]
-        elif i == 23:
-            pos[i] = [nodedata['Longitude']+0.2, nodedata['Latitude'] - 0.3]
-        elif i == 15:
-            pos[i] = [nodedata['Longitude'], nodedata['Latitude'] - 0.2]
-        elif i == 34:
-            pos[i] = [nodedata['Longitude']-0.3, nodedata['Latitude']+0.17]
-        elif i == 33:
-            pos[i] = [nodedata['Longitude']-0.15, nodedata['Latitude']]
-        elif i == 35:
-            pos[i] = [nodedata['Longitude']-0.15, nodedata['Latitude'] + 0.2]
-        else:
-            pos[i] = [nodedata['Longitude'], nodedata['Latitude']]
-        map[node] =i
-        H.add_node(i)
-        if i in end_node_list:
-            color_map.append('green')
-            nodedata['type'] = 'end_node'
-        else:
-            color_map.append([30 / 255, 144 / 255, 255 / 255])
-            nodedata['type'] = 'repeater_node'
-        i=i+1
-
-    for (u,v) in G.edges:
-        H.add_edge(map[u],map[v])
-    nx.set_node_attributes(G, pos, name='pos')
-
-    nx.set_node_attributes(H,pos,name = 'pos')
-
-    draw_graph(H,D,paths)
 
